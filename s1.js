cd58034a997e17367d0402a057578d010eeb2b0c
hoang180206 <hoangnguyen29011802@gmail.com>
2025-03-19 23:48:15 +0700

    Update s1.js

--- s1.js
+++ s1.js
@@ -1,6 +1,6 @@
-const nak=document.querySelector('#nak');
-const yena=document.querySelector('#yena');
-const h1 = document.querySelector('h1');
+let nak=document.querySelector('#nak');
+let yena=document.querySelector('#yena');
+let h1 = document.querySelector('h1');
 nak.addEventListener('click',() =>{ 
     h1.style.display = 'none';
 });
